f07de371c9dad7f02b8d2d8d4e830d39ad1bdbc3
Saravanan Ps <saravananps49@gmail.com>
2026-04-13 20:37:48 +0530

    updated admin

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -11,7 +11,7 @@ function Blogs() {
 
     const [blogs, setBlogs] = useState([]);
     // const [log, setLog] = useState("")
-    const [, setAdmin] = useState(false)
+    const [admin, setAdmin] = useState(false)
 
     useEffect(()=> 
     {
@@ -118,8 +118,8 @@ function Blogs() {
         <div className="blog-section py-14">
             <h2 className="text-center text-5xl font-bold mb-14"> <span className='text-blue-400'>Peer Project Hub</span> </h2>
 
-            {/* Blog creation form */}
-            {/* {admin? */}
+            /* Blog creation form */
+            {admin?
             <div className="blog-creation-form mb-8" style={{ width: "80%", margin: "auto" }}>
                 <form onSubmit={handleNewBlogSubmit} className="flex flex-col gap-4">
                     <input
@@ -143,9 +143,9 @@ function Blogs() {
                     </button>
                 </form>
             </div> 
-            {/* :
-            ""  */}
-            {/* } */}
+             :
+            ""  
+             }
 
             <div className="blogs-container grid grid-cols-1 md:grid-cols-2 gap-6 container mx-auto px-4">
                 {blogs.map((blog) => (
